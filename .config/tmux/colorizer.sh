31bebe0e5a9cacfd351604729d065444ba715b78
ori <orisneh@gmail.com>
2025-08-13 15:36:41 +0300

    move tmux to its own config

---
+++ .config/tmux/colorizer.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+COLORS_PATH="$HOME/.config/tmux/colors"
+DEFAULT_COLORSCHEME="tomorrow-night"
+CACHE_FILE="$HOME/.cache/tmux_colorscheme"
+
+load_from_cache() {
+    if [ -f "$CACHE_FILE" ]; then
+        tmux source-file "$(cat "$CACHE_FILE")"
+        exit 0
+    else
+        tmux source-file "$HOME/.config/tmux/colors/$DEFAULT_COLORSCHEME.conf"
+        exit 0
+    fi
+}
+
+if [ "${1:-}" == "--cache" ]; then
+    load_from_cache
+fi
+
+if command -v "fd" >/dev/null 2>&1; then
+    find_command="fd .conf \"$COLORS_PATH\""
+else
+    find_command="find \"$COLORS_PATH\" -name '*.conf'"
+fi
+
+colorschemes=$(bash -c "$find_command" | xargs -n 1 basename)
+selection=$(echo "$colorschemes" | fzf --height=60% --layout=reverse)
+selection_file="$COLORS_PATH/$selection"
+
+tmux source-file "$selection_file"
+
+echo "$selection_file" >"$CACHE_FILE"
---
+++ .config/tmux/sessionizer.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+FIND_DEPTH=5
+FIND_DIR="$HOME"
+
+excludes=(
+    .local
+    .cargo
+    .cache
+    .npm
+    node_modules
+    .virtualenvs
+    venv
+    .venv
+    __pycache__
+    .pytest_cache
+    .mypy_cache
+    .pyenv
+    target
+    dist
+    build
+    out
+)
+
+includes=(
+    ~/.config/nvim
+    ~/.config/git
+    ~/.ssh
+)
+
+if command -v "fd" >/dev/null 2>&1; then
+    find_command="fd '^.git$' $FIND_DIR $(printf -- "--exclude %s " "${excludes[@]}") --hidden --type=directory --no-ignore --max-depth ${FIND_DEPTH} -x echo {//}"
+else
+    find_command="find $FIND_DIR -maxdepth ${FIND_DEPTH} -name .git -type d $(printf -- "-not -path '*/%s/*' " "${excludes[@]}" | sed 's/ $//') -exec dirname {} \;"
+fi
+
+paths=$(bash -c "$find_command")
+for include in "${includes[@]}"; do
+    paths="$paths"$'\n'"$include"
+done
+
+if command -v "fzf" >/dev/null 2>&1; then
+    selected_dir=$(echo "$paths" | fzf --height=60% --layout=reverse --preview-window 'right,border-left' --preview 'ls {}')
+else
+    read -rp "Sessionize Directory: " dir_input
+    selected_dir=$(echo "$paths" | grep "$dir_input" | head -1)
+fi
+
+session_name=$(basename "$selected_dir" | tr . _)
+if ! tmux has-session -t="$session_name" 2>/dev/null; then
+    tmux new-session -ds "$session_name" -c "$selected_dir"
+fi
+([ -z "$TMUX" ] && tmux attach -t "$session_name") || tmux switch-client -t "$session_name"
