ea1082dc7325f06c6694f34158b888bdf313d83c
ori <orisneh@gmail.com>
2026-05-07 13:19:07 +0300

    added oil-nvim with mini-icons

--- .config/nvim/plugin/00-load.lua
+++ .config/nvim/plugin/00-load.lua
@@ -3,9 +3,15 @@ vim.pack.add({
 	"https://github.com/stevearc/conform.nvim",
 	"https://github.com/neovim/nvim-lspconfig",
 
+	-- QoL
+	"https://github.com/stevearc/oil.nvim",
+	"https://github.com/nvim-mini/mini.icons",
+
 	-- colorschemes
 	"https://github.com/irohn/koda",
 	"https://github.com/Aejkatappaja/sora",
 	"https://github.com/omacom-io/lumon.nvim",
 	"https://github.com/rebelot/kanagawa.nvim",
 })
+
+require("mini.icons").setup()
